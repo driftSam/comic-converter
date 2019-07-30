4b7341737c5d07c49f2de007bd5f53a94d92c564
pete <pjjuliana216@gmail.com>
2019-07-30 15:41:50 -0400

    Complete rename refactor

---
+++ src/main/java/com/longbox/convert/ComicConverterApplication.java
@@ -0,0 +1,74 @@
+package com.longbox.convert;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.longbox.convert.messaging.Receiver;
+
+@SpringBootApplication
+public class ComicConverterApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(ComicConverterApplication.class);
+
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	static final String queueName = "spring-boot";
+	private final RabbitTemplate rabbitTemplate;
+
+	public ComicConverterApplication(RabbitTemplate rabbitTemplate) {
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicConverterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		rabbitTemplate.convertAndSend(topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+	}
+
+}
---
+++ src/test/java/com/longbox/convert/ComicConverterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.longbox.convert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComicConverterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
